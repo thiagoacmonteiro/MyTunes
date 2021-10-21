f576088b36ce2518b1e547692d3a340acb6834e1
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-21 07:43:16 -0300

    Requirement 5 solved

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -39,6 +40,9 @@ class Header extends React.Component {
           Welcome
           { user.name }
         </p>
+        <Link to="/search" data-testid="link-to-search"> Search </Link>
+        <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
+        <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
       </header>
     );
   }
