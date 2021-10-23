14a2c571be67bad8a23aaae62fcd0f62be84f27d
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-10-23 10:36:46 -0300

    Requirement 8 solved

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favorite: false,
+      loading: false,
+    };
+
+    this.addToFavorites = this.addToFavorites.bind(this);
+  }
+
+  addToFavorites({ target }) {
+    const favorite = target.checked;
+    const {
+      music:
+       { trackId },
+    } = this.props;
+
+    this.setState({ loading: true },
+      () => addSong(trackId).then(() => this.setState({
+        favorite,
+        loading: false,
+      })));
+  }
+
   render() {
     const { music } = this.props;
+    const { loading, favorite } = this.state;
 
     return (
       <div>
@@ -18,6 +46,17 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="favorite-check">
+          Favorita
+          <input
+            data-testid={ `checkbox-music-${music.trackId}` }
+            name="favorite-check"
+            type="checkbox"
+            onClick={ this.addToFavorites }
+            checked={ favorite }
+          />
+        </label>
+        { loading ? <Loading /> : <p /> }
       </div>
     );
   }
